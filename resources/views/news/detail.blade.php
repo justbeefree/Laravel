ccecd6cef444fe5be954ebe2385faeade6ef110b
Mihail Starkov <jakcon990@gmail.com>
2023-06-01 11:49:59 +0300

    #lesson_1 первое практическое задаине.

---
+++ resources/views/news/detail.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Новость</title>
+</head>
+<body>
+<div style="text-align: center">
+    <h1>
+        Детальная страница новости
+    </h1>
+    <div>
+        Текст новости Текст новости Текст новости Текст новости Текст новости Текст новости Текст новости
+    </div>
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//страница приветствия
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//страница о проекте
+Route::get('/about/', function () {
+    return view('about');
+});
+
+//страница со списком новостей
+Route::get('/news/', function () {
+    return view('news.index');
+});
+
+//детальная страница новости
+Route::get('/news/{id}', function () {
+    return view('news.detail');
+});
