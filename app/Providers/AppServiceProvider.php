3a684c0b34c7308eb5f9b87a682fe6550c4df97c
Mihail Starkov <jakcon990@gmail.com>
2023-07-01 15:38:34 +0300

    #lesson_6 регистрация в сервис провайдере

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,9 @@ namespace App\Providers;
 use App\Models\Category;
 use App\Models\News;
 use App\Queries\CategoriesQueryBuilder;
+use App\Queries\FeedbacksQueryBuilder;
 use App\Queries\NewsQueryBuilder;
+use App\Queries\OrdersQueryBuilder;
 use App\Queries\SourcesQueryBuilder;
 use Illuminate\Support\ServiceProvider;
 use App\Queries\QueryBuilder;
@@ -20,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(QueryBuilder::class, CategoriesQueryBuilder::class);
         $this->app->bind(QueryBuilder::class, NewsQueryBuilder::class);
         $this->app->bind(QueryBuilder::class, SourcesQueryBuilder::class);
+        $this->app->bind(QueryBuilder::class, OrdersQueryBuilder::class);
+        $this->app->bind(QueryBuilder::class, FeedbacksQueryBuilder::class);
     }
 
     /**
